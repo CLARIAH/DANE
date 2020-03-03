3fee461330daf95ec04eee60441ff78eed9aed54
Nanne <n.j.e.vannoord@uva.nl>
2020-03-03 17:46:30 +0100

    Added global config

---
+++ DANE/config.py
@@ -0,0 +1,39 @@
+from yacs.config import CfgNode as CN
+import os
+
+cfg = CN()
+
+cfg.DANE = CN()
+cfg.DANE.HOST = '0.0.0.0' # host we listen on
+cfg.DANE.PORT = 5500
+if 'DANE_HOME' in os.environ.keys():
+    cfg.DANE.HOME_DIR = os.path.join(os.environ['DANE_HOME'], '')
+else:
+    cfg.DANE.HOME_DIR = os.path.join(os.environ['HOME'], ".DANE", '')
+cfg.DANE.LOCAL_DIR = os.path.join(os.getcwd(), '')
+cfg.DANE.API_URL = 'http://localhost:5500/DANE/' # URL the api can be reached at
+cfg.DANE.MANAGE_URL = 'http://localhost:5500/manage/'
+
+cfg.RABBITMQ = CN()
+cfg.RABBITMQ.HOST = 'localhost'
+cfg.RABBITMQ.PORT = 5672
+cfg.RABBITMQ.EXCHANGE = 'DANE-exchange'
+cfg.RABBITMQ.RESPONSE_QUEUE = 'DANE-response-queue'
+cfg.RABBITMQ.USER = 'guest'
+cfg.RABBITMQ.PASSWORD = 'guest'
+
+cfg.CUDA = CN()
+cfg.CUDA.VISIBLE_DEVICES = '1'
+
+# Does the home dir have a config with additional param?
+# Add them. Or override defaults defined here
+if os.path.exists(os.path.join(cfg.DANE.HOME_DIR, "config.yml")):
+    cfg.merge_from_file(os.path.join(cfg.DANE.HOME_DIR, "config.yml"))
+
+# Does the local dir have a config with additional param?
+# NOTE: global conf can overwrite the local dir, in case one might need that
+if os.path.exists(os.path.join(cfg.DANE.LOCAL_DIR, "config.yml")):
+    cfg.merge_from_file(os.path.join(cfg.DANE.LOCAL_DIR, "config.yml"))
+
+# make immutable
+cfg.freeze()
