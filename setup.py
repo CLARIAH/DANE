688521af44bd69c592524a0684f1cc6f8c32f359
Nanne <n.j.e.vannoord@uva.nl>
2020-04-01 16:33:22 +0200

    Added license file and clarified license status

--- setup.py
+++ setup.py
@@ -6,13 +6,25 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='DANE',
-    version='0.1',
+    version='0.2.1',
     author='Nanne van Noord',
     author_email='n.j.e.vannoord@uva.nl',
     url='https://github.com/CLARIAH/DANE',
     description='Utils for working with the Distributed Annotation and Enrichment system.',
     long_description=long_description,
     long_description_content_type='text/markdown',
+    license='Apache License 2.0',
+
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Multimedia :: Video",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 
     packages=find_packages(exclude=('test',)),
 
