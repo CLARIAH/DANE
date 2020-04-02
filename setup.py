76d5e503652436389b5c6ff0722f316e79344fab
Nanne <n.j.e.vannoord@uva.nl>
2020-04-02 10:07:10 +0200

    correct status

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(name='DANE',
     license='Apache License 2.0',
 
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: Apache Software License",
