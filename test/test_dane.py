1d097f80faa5c6bfcc3ecd574dff8c272495ce73
Nanne <n.j.e.vannoord@uva.nl>
2020-09-03 14:46:45 +0200

    Updated tests to 0.2

--- test/test_dane.py
+++ test/test_dane.py
@@ -9,43 +9,76 @@ from tempfile import TemporaryDirectory
 from importlib import reload
 import DANE.config
 
-class TestJob(unittest.TestCase):
-    # TODO This implicitly also tests tasks, perhaps should make this explicit
+class TestDocument(unittest.TestCase):
 
     def setUp(self):
-        self.job = DANE.Job(source_url='http://127.0.0.1/example', 
-                source_id='TEST123',
-                tasks=DANE.taskSequential(['TEST']))
+        self.doc = DANE.Document(
+            {
+                'id': 'TEST123',
+                'url': 'http://127.0.0.1/example',
+                'type': 'Text'
+            },{
+                'id': 'TEST',
+                'type': 'Software'
+            }
+        ) 
 
         self.dummy = DummyHandler()
 
     def test_serialize(self):
-        serialised = self.job.to_json()
+        serialised = self.doc.to_json()
         self.assertIsInstance(serialised, str)
 
-        new_job = DANE.Job.from_json(serialised)
-        self.assertIsInstance(new_job, DANE.Job)
+        new_doc = DANE.Document.from_json(serialised)
+        self.assertIsInstance(new_doc, DANE.Document)
 
-        self.assertEqual(self.job.source_id, new_job.source_id)
-        self.assertEqual(self.job.source_url, new_job.source_url)
-        self.assertEqual(self.job.tasks.to_json(), new_job.tasks.to_json())
+        self.assertEqual(self.doc.target['id'], new_doc.target['id'])
+        self.assertEqual(self.doc.creator['id'], new_doc.creator['id'])
 
-    def test_register_and_run(self):
-        self.job.set_api(self.dummy)
-        self.assertIsInstance(self.job.api, DummyHandler)
+    def test_register(self):
+        self.doc.set_api(self.dummy)
+        self.assertIsInstance(self.doc.api, DummyHandler)
         
-        self.job.apply(lambda x: (self.assertIsInstance(x.api, DummyHandler)))
-        # API is good to go!
+        self.doc.register()
 
-        self.job.register()
+        self.assertIsNotNone(self.doc._id)
 
-        self.assertIsNotNone(self.job.job_id)
-        self.job.apply(lambda x: (self.assertEqual(x.job_id, self.job.job_id)))
-        self.job.apply(lambda x: (self.assertIsNotNone(x.task_id)))
-        # job is registered, tasks are registered
+class TestTask(unittest.TestCase):
 
-        self.job.run()
-        self.assertTrue(self.job.isDone())
+    def setUp(self):
+        self.task = DANE.Task('TEST')
+        self.dummy = DummyHandler()
+        
+        # depend on doc being able to register for testing tasks
+        self.doc = DANE.Document(
+            {
+                'id': 'TEST123',
+                'url': 'http://127.0.0.1/example',
+                'type': 'Text'
+            },{
+                'id': 'TEST',
+                'type': 'Software'
+            }
+        ) 
+        self.doc.set_api(self.dummy)
+        self.doc.register()
+
+    def test_serialize(self):
+
+        serialised = self.task.to_json()
+        self.assertIsInstance(serialised, str)
+
+        new_task = DANE.Task.from_json(serialised)
+        self.assertIsInstance(new_task, DANE.Task)
+
+        self.assertEqual(self.task.key, new_task.key)
+        self.assertEqual(self.task.priority, new_task.priority)
+
+    def test_assign(self):
+        self.task.set_api(self.dummy)
+
+        self.task.assign(self.doc._id)
+
 
 class TestConfig(unittest.TestCase):
     
@@ -73,7 +106,6 @@ class TestConfig(unittest.TestCase):
 
         reload(DANE.config)
         cfg = DANE.config.cfg
-
 
         self.assertIsInstance(cfg.TEST, CfgNode)
         self.assertEqual(cfg.TEST.SCOPE, "LOCAL")
