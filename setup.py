275cbff64923e048a91469b7ac0814aea9a15905
Nanne <n.j.e.vannoord@uva.nl>
2020-11-05 16:00:13 +0100

    Up version for pypi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='DANE',
-    version='0.2.1',
+    version='0.2.2',
     author='Nanne van Noord',
     author_email='n.j.e.vannoord@uva.nl',
     url='https://github.com/CLARIAH/DANE',
