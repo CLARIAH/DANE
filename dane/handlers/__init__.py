0b23926638adc3bea7599df0c8174dc913f7e6cc
jblom <jblom@beeldengeluid.nl>
2022-09-19 14:47:48 +0200

    readded module import simplifications for ESHandler and RMQHandler

---
+++ dane/handlers/__init__.py
@@ -0,0 +1,5 @@
+# for simplifying imports, e.g.:
+# `from dane.handlers import ESHandler` instead of:
+# `from dane.handlers.ESHandler import ESHandler`
+from .ESHandler import ESHandler
+from .RabbitMQHandler import RabbitMQHandler
